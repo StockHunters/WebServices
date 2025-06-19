e351d2819ab6cc51223bbb7e73e8e51492323414
jsyumg <117481269+jsyumg@users.noreply.github.com>
2025-06-19 08:09:36 -0500

    feat: category update

---
+++ Web-Services/InventoryManagement/Interfaces/REST/Transform/CreateCategoryCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using Web_Services.InventoryManagement.Domain.Model.Commands;
+using Web_Services.InventoryManagement.Interfaces.REST.Resources;
+
+namespace Web_Services.InventoryManagement.Interfaces.REST.Transform;
+
+public static class CreateCategoryCommandFromResourceAssembler
+{
+    public static CreateCategoryCommand ToCommandFromResource(CreateCategoryResource resource)
+    {
+        return new CreateCategoryCommand(resource.Name, resource.Description);
+    }
+}
