e9b1a103925246268e8494dc6c88c34c52d4a6c2
Suin Yum <u202210838@upc.edu.pe>
2025-06-19 21:03:58 -0500

    feat: inventory adjustments controller

---
+++ Web-Services/InventoryManagement/Domain/Services/IInventoryAdjustmentQueryService.cs
@@ -0,0 +1,10 @@
+﻿using Web_Services.InventoryManagement.Domain.Model.Aggregates;
+using Web_Services.InventoryManagement.Domain.Model.Queries;
+
+namespace Web_Services.InventoryManagement.Domain.Services;
+
+public interface IInventoryAdjustmentQueryService
+{
+    Task<IEnumerable<InventoryAdjustment>> Handle(GetAllInventoryAdjustmentsQuery query);
+    Task<InventoryAdjustment?> Handle(GetInventoryAdjustmentByIdQuery query);
+}
